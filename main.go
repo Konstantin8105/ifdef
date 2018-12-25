4bd8959e19abbc74f40fe263981224f8dd432591
Konstantin8105 <konstantin8105@gmail.com>
2018-12-25 14:16:56 +0300

    add pattern implementation

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"sort"
 	"strings"
 )
 
 var osStdout *os.File = os.Stdout
 
 const (
-	beginToken  = "#ifdef"
-	finishToken = "#endif"
+	ifdefToken = "#ifdef"
+	kvToken    = "#kv"
+	endToken   = "#endif"
 )
 
 var (
@@ -80,11 +82,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: %v", err)
 		return
 	}
-
-	// gofmt
-	_, _ = exec.Command("gofmt", "-s", "-w", *outputFile).CombinedOutput()
-	// goimports
-	_, _ = exec.Command("goimports", "-w", *outputFile).CombinedOutput()
 }
 
 // list of preprocessors
@@ -98,18 +95,18 @@ func list() error {
 	var names []string
 	for i := range lines {
 		// filter
-		if !bytes.Contains(lines[i], []byte(beginToken)) {
+		if !bytes.Contains(lines[i], []byte(ifdefToken)) {
 			continue
 		}
 
 		// find name
-		index := bytes.Index(lines[i], []byte(beginToken))
+		index := bytes.Index(lines[i], []byte(ifdefToken))
 		if index < 0 {
 			continue
 		}
 
 		// get name
-		name := string(lines[i][index+len(beginToken):])
+		name := string(lines[i][index+len(ifdefToken):])
 		ls := strings.Split(name, " ")
 		for j := range ls {
 			ls[j] = strings.TrimSpace(ls[j])
@@ -122,7 +119,17 @@ func list() error {
 
 	// show names
 	fmt.Fprintf(osStdout, "Preprocessor names :\n")
-	for i := range names {
+	sort.Strings(names)
+	for i := 0; i < len(names); i++ {
+		isUniq := true
+		for j := i + 1; j < len(names); j++ {
+			if names[i] == names[j] {
+				isUniq = false
+			}
+		}
+		if !isUniq {
+			continue
+		}
 		fmt.Fprintf(osStdout, "* %s\n", names[i])
 	}
 
@@ -140,21 +147,33 @@ func change() error {
 	lines := bytes.Split(b, []byte("\n"))
 	var buf bytes.Buffer
 	addLine := true
+
+	kv := map[string]string{}
+
 	for i := range lines {
-		// beginToken
-		index := bytes.Index(lines[i], []byte(beginToken))
-		if index >= 0 {
-			// get name
-			name := strings.TrimSpace(string(lines[i][index+len(beginToken):]))
-			addLine = true
-			if name == ps {
-				addLine = false
+		line := string(lines[i])
+
+		// ifdefToken
+		has, found := hasIfdef(line, ps)
+		if has {
+			addLine = false
+			if found {
+				addLine = true
 			}
 			continue
 		}
 
-		// finishToken
-		index = bytes.Index(lines[i], []byte(finishToken))
+		// kvToken
+		has, found, key, value := hasKv(line, ps)
+		if has {
+			if found {
+				kv[key] = value
+			}
+			continue
+		}
+
+		// endToken
+		index := strings.Index(line, endToken)
 		if index >= 0 {
 			// get name
 			addLine = true
@@ -165,9 +184,71 @@ func change() error {
 			continue
 		}
 
-		buf.Write(lines[i])
+		// key-value changing
+		for k, v := range kv {
+			line = strings.Replace(line, "#"+k, v, -1)
+		}
+
+		buf.WriteString(line)
 		buf.Write([]byte("\n"))
 	}
 
-	return ioutil.WriteFile(*outputFile, buf.Bytes(), 0644)
+	err = ioutil.WriteFile(*outputFile, buf.Bytes(), 0644)
+	if err != nil {
+		return err
+	}
+
+	// gofmt
+	_, _ = exec.Command("gofmt", "-s", "-w", *outputFile).CombinedOutput()
+	// goimports
+	_, _ = exec.Command("goimports", "-w", *outputFile).CombinedOutput()
+
+	return nil
+}
+
+func hasIfdef(line, ps string) (has, found bool) {
+	index := strings.Index(line, ifdefToken)
+	if index >= 0 {
+		has = true
+		preprocessors := strings.Split(line[index+len(ifdefToken):], " ")
+		for i := range preprocessors {
+			preprocessors[i] = strings.TrimSpace(preprocessors[i])
+			if preprocessors[i] == "" {
+				continue
+			}
+			if preprocessors[i] == ps {
+				found = true
+				break
+			}
+		}
+	}
+	return
+}
+
+func hasKv(line, ps string) (has, found bool, key, value string) {
+	index := strings.Index(line, kvToken)
+	if index >= 0 {
+		has = true
+		// example : preprocessors = "Float32 keys:value"
+		preprocessors := strings.TrimSpace(line[index+len(kvToken):])
+		index = strings.Index(preprocessors, " ")
+		if index < 0 {
+			return
+		}
+		if preprocessors[:index] != ps {
+			return
+		}
+		found = true
+		preprocessors = preprocessors[index:]
+
+		// example : preprocessors = "keys:value"
+		preprocessors = strings.TrimSpace(preprocessors)
+		index = strings.Index(preprocessors, ":")
+		if index < 0 {
+			return
+		}
+		key = preprocessors[:index]
+		value = preprocessors[index+1:]
+	}
+	return
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"strings"
 	"testing"
 )
 
 func Test(t *testing.T) {
+	*inputFile = "./testdata/test.gotmpl"
+
 	t.Run("list", func(t *testing.T) {
-		*inputFile = "./testdata/test.gotmpl"
 		*listFlag = true
 		defer func() {
 			*listFlag = false
@@ -41,9 +43,102 @@ func Test(t *testing.T) {
 		}
 
 		s1 := string(b)
-		s2 := "Preprocessor names :\n* pre1\n* pre2\n* Float64\n* Float32\n"
+		s2 := "Preprocessor names :\n* Float32\n* Float64\n* pre1\n* pre2\n"
 		if strings.Compare(s1, s2) != 0 {
 			t.Fatalf("Not equal:\n%s\n%s", s1, s2)
 		}
 	})
+
+	t.Run("preprocessors", func(t *testing.T) {
+		tcs := []struct {
+			outputFile string
+			pres       string
+		}{
+			{
+				outputFile: "./testdata/pre1.go",
+				pres:       "pre1",
+			},
+			{
+				outputFile: "./testdata/pre2.go",
+				pres:       "pre2",
+			},
+			{
+				outputFile: "./testdata/f32.go",
+				pres:       "Float32",
+			},
+			{
+				outputFile: "./testdata/f64.go",
+				pres:       "Float64",
+			},
+		}
+		defer func() {
+			*outputFile = ""
+			*pres = ""
+		}()
+
+		for _, tc := range tcs {
+			t.Run(tc.pres, func(t *testing.T) {
+				*outputFile = tc.outputFile
+				*pres = tc.pres
+				err := change()
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				// compare results
+				out, err := ioutil.ReadFile(tc.outputFile)
+				if err != nil {
+					t.Fatal(err)
+				}
+				exp, err := ioutil.ReadFile(tc.outputFile + ".expect")
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !bytes.Equal(out, exp) {
+					t.Fatalf("result is not same")
+				}
+			})
+		}
+	})
+}
+
+func TestIfDef(t *testing.T) {
+	tcs := []struct {
+		line, ps   string
+		has, found bool
+	}{
+		{
+			line:  "// #ifdef FL",
+			ps:    "FL",
+			has:   true,
+			found: true,
+		},
+		{
+			line:  "// #ifdef FL FL2 DL",
+			ps:    "FL",
+			has:   true,
+			found: true,
+		},
+		{
+			line:  "// #ifdef FL3 FL2 DL1",
+			ps:    "FL",
+			has:   true,
+			found: false,
+		},
+		{
+			line:  "// #ifdef FL3 FL DL",
+			ps:    "FL",
+			has:   true,
+			found: true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.line, func(t *testing.T) {
+			has, _ := hasIfdef(tc.line, tc.ps)
+			if has != tc.has {
+				t.Errorf("not same")
+			}
+		})
+	}
 }
